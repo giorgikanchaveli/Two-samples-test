413be2cf128f21bd596b0705e6aff9cd9b1674c7
giorgikanchaveli <gkanchaveli99@gmail.com>
2025-02-05 14:36:31 +0100

    estim file, added function to plot

--- simulations/estimation.jl
+++ simulations/estimation.jl
@@ -18,7 +18,6 @@ function int_wass_beta(p_a, p_b, q_a, q_b)
     cdf_q = x -> cdf(q, x)
     f = x -> abs(cdf_p(x) - cdf_q(x))
     return quadgk(f, 0, 1)[1]
-    return wass(p, q)
 end
 
 
@@ -56,51 +55,194 @@ function get_distance(p::PPM, q::PPM, n::Int, m::Int, s::Int)
 end
 
 
+function estimation_plot(rpm::String, same::Bool, n_top::Int, n_bottom::Int, s::Int)
+    # this function computes the distances between the empirical measures
+    # sampled directly from p and q and returns plots and summary statistics
+
+    # rpm: "same/splitting or Beta
+    # same: Boolean varable to denote if the measures are the same or not
+    # s : number of samples
+    # n,m : n_top, n_bottom
+    if rpm == "samesplitting"
+        if same
+            p_1 = ()->probability("same") # uniform(-1/2, 1/2)
+            p_2 = ()->probability("same") # uniform(-1/2, 1/2)
+            d_true = 0.0
+        else
+            p_1 = ()->probability("same") # uniform(-1/2, 1/2)
+            p_2 = ()->probability("splitting") # 1/2 uniform(-1, -3/4) + 1/2 uniform(3/4, 1)
+            d_true = quadgk(x -> abs(cdf_same(x) - cdf_splitting(x)), -1, 1)[1]
+
+        end
+        
+    else
+        p_a, p_b, q_a, q_b = 1.2, 0.3, 1.1, 3.4
+        if same
+            p_1 = ()->rand(Beta(p_a, p_b))
+            p_2 = ()->rand(Beta(p_a, p_b))
+            d_true = 0.0
+        else
+            p_1 = ()->rand(Beta(p_a, p_b))
+            p_2 = ()->rand(Beta(q_a, q_b))
+            d_true = round(int_wass_beta(p_a, p_b, q_a, q_b), digits = 6)
+        end
+    end
+    α = 1.0
+    dp_1 = DP(α, p_1, -1.0, 1.0)
+    dp_2 = DP(α, p_2, -1.0, 1.0)
+
+    @assert n_top == 16 || n_top == 128 "n_top must be or 16 or  128"
+    @assert n_bottom == 5000 "n_bottom must be 5000"
+    d_ww, d_lip = get_distance(dp_1, dp_2, n_top, n_bottom, s)
+
+    summary = plot(title = "summary")
+    
+    sc = plot(title = "Test statistics, ww vs dlip", xlabel = "sample", ylabel = "distance")
+    scatter!(sc, d_ww, label = "ww", color = "red")
+    scatter!(sc, d_lip, label = "dlip", color = "blue")
+    hline!(sc, [mean(d_ww)], label="mean ww", color="red")
+    hline!(sc, [mean(d_lip)], label="mean dlip", color="blue")
+    if same==false
+        hline!(sc, [d_true], label="true distance", color="green")
+    end
+    var_ww = round(var(d_ww), digits=6)
+    var_lip = round(var(d_lip), digits=6)
+    scatter!(summary, [], [], label="Var_ww = $(var_ww)")  # insert text variance for ww
+    scatter!(summary, [], [], label="Var_dlip = $(var_lip)")  # insert text variance for dlip
+    scatter!(summary, [], [], label="d_true = $(d_true)")  # insert text for int_estimate of distance*sqrt(n/2)
+    
+    bias_ww = round(mean(d_ww) - d_true, digits=6)
+    bias_lip = round(mean(d_lip) - d_true, digits=6)
+    scatter!(summary, [], [], label="Bias_ww = $(bias_ww)")  # insert text bias for ww
+    scatter!(summary, [], [], label="Bias_dlip = $(bias_lip)")  # insert text bias for dlip
+    return sc, summary
+end
+
 # Define measures
 
-# p_1 = ()->probability("same")
-# p_2 = ()->probability("splitting")
+# p_1 = ()->probability("same") # uniform(-1/2, 1/2)
+# p_2 = ()->probability("splitting") # 1/2 uniform(-1, -3/4) + 1/2 uniform(3/4, 1)
 # dp_1 = DP(1.0, p_1, -1.0, 1.0)
 # dp_2 = DP(1.0, p_2, -1.0, 1.0)
-p_a, p_b = 1.2, 0.3
-q_a, q_b = 1.1, 3.4
-beta_p, beta_q = Beta(p_a, p_b), Beta(q_a, q_b)
-p_1 = ()->rand(beta_p)
-p_2 = ()->rand(beta_q)
-α = 1.0
-dp_1 = DP(α, p_1, 0.0, 1.0)
-dp_2 = DP(α, p_2, 0.0, 1.0)
-n_top, n_bottom = 50, 10
+# p_a, p_b = 1.2, 0.3
+# q_a, q_b = 1.1, 3.4
+# beta_p, beta_q = Beta(p_a, p_b), Beta(q_a, q_b)
+# p_1 = ()->rand(beta_p)
+# p_2 = ()->rand(beta_q)
+# α = 1.0
+# dp_1 = DP(α, p_1, -1.0, 1.0)
+# dp_2 = DP(α, p_2, -1.0, 1.0)
+# n_top, n_bottom = 128, 5000
 
-Random.seed!(1234)
+# @assert n_top == 16 || n_top == 128 "n_top must be or 16 or  128"
+# @assert n_bottom == 5000 "n_bottom must be 5000"
+
+# Random.seed!(1234)
 # compute distances using direct samples
-s = 24
-d_ww, d_lip = get_distance(dp_1, dp_2, n_top, n_bottom, s)
+#s = 24
+# d_ww, d_lip = get_distance(dp_1, dp_2, n_top, n_bottom, s)
 #d_mc = mc_wass_beta(p_a, p_b, q_a, q_b, 1000000,100)
-d_int = round(int_wass_beta(p_a, p_b, q_a, q_b), digits = 6)
+#d_true = round(int_wass_beta(p_a, p_b, q_a, q_b), digits = 6)
+#d_true = quadgk(x -> abs(cdf_same(x) - cdf_splitting(x)), -1, 1)[1]
 
 
-summary = plot(title = "summary")
-sc = plot(title = "Test statistics, ww vs dlip", xlabel = "sample", ylabel = "distance")
-scatter!(sc, d_ww, label = "ww", color = "red")
-scatter!(sc, d_lip, label = "dlip", color = "blue")
-hline!(sc, [mean(d_ww)], label="mean ww", color="red")
-hline!(sc, [mean(d_lip)], label="mean dlip", color="blue")
-hline!(sc, [d_int], label="int_estimate", color="green")
-var_ww = round(var(d_ww), digits=6)
-var_lip = round(var(d_lip), digits=6)
-scatter!(summary, [], [], label="Var_ww = $(var_ww)")  # insert text variance for ww
-scatter!(summary, [], [], label="Var_dlip = $(var_lip)")  # insert text variance for dlip
-scatter!(summary, [], [], label="d_int = $(d_int)")  # insert text for int_estimate of distance*sqrt(n/2)
+# summary = plot(title = "summary")
+# sc = plot(title = "Test statistics, ww vs dlip", xlabel = "sample", ylabel = "distance")
+# scatter!(sc, d_ww, label = "ww", color = "red")
+# scatter!(sc, d_lip, label = "dlip", color = "blue")
+# hline!(sc, [mean(d_ww)], label="mean ww", color="red")
+# hline!(sc, [mean(d_lip)], label="mean dlip", color="blue")
+# hline!(sc, [d_true], label="true distance", color="green")
+# var_ww = round(var(d_ww), digits=6)
+# var_lip = round(var(d_lip), digits=6)
+# scatter!(summary, [], [], label="Var_ww = $(var_ww)")  # insert text variance for ww
+# scatter!(summary, [], [], label="Var_dlip = $(var_lip)")  # insert text variance for dlip
+# scatter!(summary, [], [], label="d_true = $(d_true)")  # insert text for int_estimate of distance*sqrt(n/2)
 
-bias_ww = round(mean(d_ww) - d_int, digits=6)
-bias_lip = round(mean(d_lip) - d_int, digits=6)
-scatter!(summary, [], [], label="Bias_ww = $(bias_ww)")  # insert text bias for ww
-scatter!(summary, [], [], label="Bias_dlip = $(bias_lip)")  # insert text bias for dlip
+# bias_ww = round(mean(d_ww) - d_true, digits=6)
+# bias_lip = round(mean(d_lip) - d_true, digits=6)
+# scatter!(summary, [], [], label="Bias_ww = $(bias_ww)")  # insert text bias for ww
+# scatter!(summary, [], [], label="Bias_dlip = $(bias_lip)")  # insert text bias for dlip
+
+
 
 
 # save figures
 
-# filepath = joinpath(pwd(),"plots/n = $(n_top), m = $(n_bottom)")
-# savefig(sc, joinpath(filepath, "estimation_ww_lip_$(n_top)_$(n_bottom)"))
-# savefig(summary, joinpath(filepath, "summary_ww_lip_$(n_top)_$(n_bottom)"))
+
+
+# everything for n = 16, m = 5000
+println("n = 16, m = 5000, Beta, same")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("Beta", true, 16, 5000, 24)
+filepath = joinpath(filepath, "beta/n = 16, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_beta_same_ww_lip_16_5000"))
+savefig(summary, joinpath(filepath, "summary_beta_same_ww_lip_16_5000"))
+
+
+println("n = 16, m = 5000, Beta, different")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("Beta", false, 16, 5000, 24)
+filepath = joinpath(filepath, "beta/n = 16, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_beta_diff_ww_lip_16_5000"))
+savefig(summary, joinpath(filepath, "summary_beta_diff_ww_lip_16_5000"))
+
+println("n = 16, m = 5000, samesplitting, same")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("samesplitting", true, 16, 5000, 24)
+filepath = joinpath(filepath, "samesplitting/n = 16, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_smspl_same_ww_lip_16_5000"))
+savefig(summary, joinpath(filepath, "summary_smspl_same_ww_lip_16_5000"))
+
+println("n = 16, m = 5000, samesplitting, different")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("samesplitting", false, 16, 5000, 24)
+filepath = joinpath(filepath, "samesplitting/n = 16, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_smspl_diff_ww_lip_16_5000"))
+savefig(summary, joinpath(filepath, "summary_smspl_diff_ww_lip_16_5000"))
+
+# everything for n = 128, m = 5000
+
+println("n = 128, m = 5000, Beta, same")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("Beta", true, 128, 5000, 24)
+filepath = joinpath(filepath, "beta/n = 128, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_beta_same_ww_lip_128_5000"))
+savefig(summary, joinpath(filepath, "summary_beta_same_ww_lip_128_5000"))
+
+println("n = 128, m = 5000, Beta, different")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("Beta", false, 128, 5000, 24)
+filepath = joinpath(filepath, "beta/n = 128, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_beta_diff_ww_lip_128_5000"))
+savefig(summary, joinpath(filepath, "summary_beta_diff_ww_lip_128_5000"))
+
+println("n = 128, m = 5000, samesplitting, same")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("samesplitting", true, 128, 5000, 24)
+filepath = joinpath(filepath, "samesplitting/n = 128, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_smspl_same_ww_lip_128_5000"))
+savefig(summary, joinpath(filepath, "summary_smspl_same_ww_lip_128_5000"))
+
+println("n = 128, m = 5000, samesplitting, different")
+filepath = joinpath(pwd(),"plots/estimation")
+sc, summary = estimation_plot("samesplitting", false, 128, 5000, 24)
+filepath = joinpath(filepath, "samesplitting/n = 128, m = 5000")
+savefig(sc, joinpath(filepath, "estimation_smspl_diff_ww_lip_128_5000"))
+savefig(summary, joinpath(filepath, "summary_smspl_diff_ww_lip_128_5000"))
+
+
+
+
+# save figures
+
+# filepath = joinpath(pwd(),"plots/beta/n = $(n_top), m = $(n_bottom)")
+# savefig(sc, joinpath(filepath, "estimation_beta_ww_lip_$(n_top)_$(n_bottom)"))
+# savefig(summary, joinpath(filepath, "summary_beta_ww_lip_$(n_top)_$(n_bottom)"))
+
+# filepath = joinpath(pwd(),"plots/samesplitting/n = $(n_top), m = $(n_bottom)")
+# savefig(sc, joinpath(filepath, "estimation_splsm_ww_lip_$(n_top)_$(n_bottom)"))
+# savefig(summary, joinpath(filepath, "summary_splsm_ww_lip_$(n_top)_$(n_bottom)"))
+
+
+
