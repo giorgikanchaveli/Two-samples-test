b9a2b1b39550e933d7fc1ec3416d4ba04fa44b40
giorgikanchaveli <gkanchaveli99@gmail.com>
2025-02-05 11:15:24 +0100

    generates fig 1

--- simulations/fig1.jl
+++ simulations/fig1.jl
@@ -48,38 +48,65 @@ end
 d_true = quadgk(x -> abs(cdf_1(x) - cdf_2(x)), -1, 1)[1]
 
 
-
-α = 1.0
-dp_1, dp_2 = DP(α, p_1, -1.0, 1.0), DP(α, p_2, -1.0, 1.0)
-
 n_bottom = 5000
-n_tops = collect(10:20:500)
+n_tops = [16,32,64,128,256]
 s = 24
-
-d_ww, d_lip = zeros(length(n_tops)), zeros(length(n_tops))
-
-for i in 1:length(n_tops)
-    n_top = n_tops[i]
-    println("n_top: ", n_top)
-    for j in 1:s
-        if j % 10 == 0
-            println("j: ", j)
-        end
-        emp_p, emp_q = generate_emp(dp_1, n_top, n_bottom), generate_emp(dp_2, n_top, n_bottom)
-        d_ww[i] += ww(emp_p, emp_q)
-        d_lip[i] += dlip(emp_p, emp_q)
+function values_fig_1(n_tops, n_bottom, s, str)
+    if str == "diff"
+        α = 1.0
+        dp_1, dp_2 = DP(α, p_1, -1.0, 1.0), DP(α, p_2, -1.0, 1.0)
+    else
+        α = 1.0
+        p = () -> rand()
+        dp_1, dp_2 = DP(α, p, 0.0, 1.0), DP(α, p, 0.0, 1.0)
     end
-    d_ww[i] /= s
-    d_lip[i] /= s
+
+    d_ww, d_lip, lower_bounds = zeros(length(n_tops)), zeros(length(n_tops)), zeros(length(n_tops))
+    for i in 1:length(n_tops)
+        n_top = n_tops[i]
+        println("n_top: ", n_top)
+        for j in 1:s
+            if j % 10 == 0
+                println("j: ", j)
+            end
+            emp_p, emp_q = generate_emp(dp_1, n_top, n_bottom), generate_emp(dp_2, n_top, n_bottom)
+            d_ww[i] += ww(emp_p, emp_q)
+            d_lip[i] += dlip(emp_p, emp_q)
+            lower_bounds[i] += lower_bound(emp_p, emp_q)
+        end
+        d_ww[i] /= s
+        d_lip[i] /= s
+        lower_bounds[i] /= s
+    end
+    return d_ww, d_lip, lower_bounds
 end
+
+
+
+
 d_trues = [d_true for i in 1:length(n_tops)]
-pl = plot(xlabel = "n", ylabel = "distance")
-scatter!(pl, n_tops, d_ww, label = "ww")
-scatter!(pl, n_tops, d_lip, label = "dlip")
-scatter!(pl, n_tops, d_trues, label = "true value")
+d_ww, d_lip, lower_bounds = values_fig_1(n_tops, n_bottom, s, "diff")
+pl_1 = plot(title = "same", xlabel = "n", ylabel = "distance")
+plot!(pl_1, n_tops, d_ww, seriestype=:line, marker=:circle, label="ww", color = "red")
+plot!(pl_1, n_tops, d_lip, seriestype=:line, marker=:circle, label="dlip", color = "blue")
+plot!(pl_1, n_tops, d_trues, seriestype=:line, marker=:circle, label="true", color = "black")
+plot!(pl_1, n_tops, lower_bounds, seriestype=:line, marker=:circle, label="lower bound", color = "green")
+
+
+d_ww, d_lip, lower_bounds = values_fig_1(n_tops, n_bottom, s, "same")
+pl_2 = plot(title = "different", xlabel = "n", ylabel = "distance")
+plot!(pl_2, n_tops, d_ww, seriestype=:line, marker=:circle, label="ww", color = "red")
+plot!(pl_2, n_tops, d_lip, seriestype=:line, marker=:circle, label="dlip", color = "blue")
+plot!(pl_2, n_tops, lower_bounds, seriestype=:line, marker=:circle, label="lower bound", color = "green")
 
 
 
 
-filepath = joinpath(pwd(),"plots")
-savefig(pl, joinpath(filepath, "fig_1_$(n_top)_$(n_bottom)"))
+
+
+
+
+
+# filepath = joinpath(pwd(),"plots")
+# savefig(pl_1, joinpath(filepath, "fig_1_left"))
+# savefig(pl_2, joinpath(filepath, "fig_1_right"))
