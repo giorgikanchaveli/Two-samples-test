87839eeac2d70b59f22b7b3f2416729b3c5e76bd
giorgikanchaveli <gkanchaveli99@gmail.com>
2025-02-11 15:26:04 +0100

    for simulating samples, in the definition of dp, I specif correct intervals for samples space

--- simulations/distances_samples.jl
+++ simulations/distances_samples.jl
@@ -28,8 +28,8 @@ function direct_sampling(p::PPM, q::PPM, n_top::Int, n_bottom::Int, nReps::Int)
             println("iteration (direct): $i")
         end
         p_emp, q_emp = generate_emp(p, n_top, n_bottom), generate_emp(q, n_top, n_bottom)
-        d_ww[i] = ww(p_emp, q_emp)
-        d_lip[i] = dlip(p_emp, q_emp)
+        d_ww[i] = ww(p_emp, q_emp) # copy(p_emp) isn't necessary
+        d_lip[i] = dlip(p_emp, q_emp) # copy(p_emp) isn't necessary
     end
     return d_ww, d_lip
 end
@@ -48,31 +48,31 @@ function permuted_sampling(p_emp::emp_ppm, q_emp::emp_ppm, nReps::Int)
         p_emp_shuffled = emp_ppm(total_atoms[indices[1:p_emp.n],:], p_emp.n, p_emp.m , p_emp.a, p_emp.b)
         q_emp_shuffled = emp_ppm(total_atoms[indices[p_emp.n+1:end],:], q_emp.n, q_emp.m, q_emp.a, q_emp.b)
 
-        d_ww[i] = ww(p_emp_shuffled, q_emp_shuffled)
-        d_lip[i] = dlip(p_emp_shuffled, q_emp_shuffled)
+        d_ww[i] = ww(p_emp_shuffled, q_emp_shuffled) # copy(p_emp_shuffed) isn't necessary
+        d_lip[i] = dlip(p_emp_shuffled, q_emp_shuffled) # copy(p_emp_shuffed) isn't necessary
     end
     return d_ww, d_lip
 end
 
-p_a, p_b, q_a, q_b = 1.2, 0.3, 1.1, 3.4
-p_1 = ()->rand(Beta(p_a, p_b))
-p_2 = ()->rand(Beta(q_a, q_b))
-d_true = round(int_wass_beta(p_a, p_b, q_a, q_b), digits = 6)
+# p_a, p_b, q_a, q_b = 1.2, 0.3, 1.1, 3.4
+# p_1 = ()->rand(Beta(p_a, p_b))
+# p_2 = ()->rand(Beta(p_a, p_b + .2))
+# d_true = round(int_wass_beta(p_a, p_b, q_a, q_b), digits = 6)
 
 
 # methods for plotting histograms
 
-function plot_hist(d_ww, d_lip, str)
+function plot_hist(d_ww, d_lip, title)
     x_max = maximum([maximum(d_ww), maximum(d_lip)]) # sets the limit of the x-axis 
     x_min = minimum([minimum(d_ww), minimum(d_lip)])
     
     h_ww = histogram(d_ww, label="ww", xlabel="distance", ylabel="frequency",
-                    title=str,
+                    title=title,
                     xticks=0.0:0.1:1.3, xlims = (0, x_max),bins = 30)
     vline!(h_ww, [mean(d_ww)], label="mean = $(round(mean(d_ww),digits = 5))", color="red")
 
     h_lip = histogram(d_lip, label="lip", xlabel="distance", ylabel="frequency",
-                    title=str,
+                    title=title,
                     xticks=0.0:0.1:1.3, xlims = (0, x_max), bins = 30)
         
     vline!(h_lip, [mean(d_lip)], label="mean = $(round(mean(d_lip), digits = 5))", color="red")
@@ -82,7 +82,7 @@ end
 function plot_quantiles(d_ww, d_lip, title, labels)
     # plot quantiles of the distances from ww and lip
     θs = collect(0.0:0.01:1.0)
-    q_plot = plot(title=title,)
+    q_plot = plot(title=title)
     plot!(q_plot, θs, quantile(d_ww, θs), xlabel="probability", ylabel="quantiles", label = labels[1])
     plot!(q_plot, θs, quantile(d_lip, θs), xlabel="probability", ylabel="quantiles", label = labels[2])
     return q_plot
@@ -99,26 +99,30 @@ function plot_vectors(v::Vector{Vector{Float64}}, title, labels)
 end
 
 function figures(measures::String, n_top::Int, n_bottom::Int, nReps::Int)
+    ℵ = 1.0
     if measures == "samesplitting"
         p_1 = ()->probability("same") # uniform(-1/2, 1/2)
         p_2 = ()->probability("splitting") # 1/2 uniform(-1, -3/4) + 1/2 uniform(3/4, 1)
+        dp_1 = DP(α, p_1, -1.0, 1.0)
+        dp_2 = DP(α, p_2, -1.0, 1.0)
         #d_true = quadgk(x -> abs(cdf_same(x) - cdf_splitting(x)), -1, 1)[1]
-    elseif measures == "betafaraway"
+    elseif measures == "betafaraway" # both distances = 0.556
         p_a, p_b, q_a, q_b = 1.2, 0.3, 1.1, 3.4
         p_1 = ()->rand(Beta(p_a, p_b))
         p_2 = ()->rand(Beta(q_a, q_b))
+        dp_1 = DP(α, p_1, 0.0, 1.0)
+        dp_2 = DP(α, p_2, 0.0, 1.0)
         #d_true = round(int_wass_beta(p_a, p_b, q_a, q_b), digits = 6)
     else
-        p_a, p_b, q_a, q_b = 1.2, 1.2, 1.2, 1.25
+        p_a, p_b, q_a, q_b = 1.2, 1.2, 1.2, 1.25 # both distances = 0.01
         p_1 = ()->rand(Beta(p_a, p_b))
         p_2 = ()->rand(Beta(q_a, q_b))
+        dp_1 = DP(α, p_1, 0.0, 1.0)
+        dp_2 = DP(α, p_2, 0.0, 1.0)
         #d_true = round(int_wass_beta(p_a, p_b, q_a, q_b), digits = 6)
     end
 
-    # define the measures
-    α = 1.0
-    dp_1 = DP(α, p_1, -1.0, 1.0)
-    dp_2 = DP(α, p_2, -1.0, 1.0)
+    
 
     # compute distances using direct samples
     d_ww, d_lip = direct_sampling(dp_1, dp_2, n_top, n_bottom, nReps)
@@ -166,11 +170,11 @@ end
 # check the output of save_fig
 
 #h, q_plot, thresh_quant_plots, thresh_rej_plots = figures("betaclose", 16, 5000, 24)
-t = time()
-save_figures(16, 5000, 24)
-println("finished n = 16")
-save_figures(128, 5000, 24)
-t = time() - t
+# t = time()
+# save_figures(16, 5000, 24)
+# println("finished n = 16")
+# save_figures(128, 5000, 24)
+# t = time() - t
 # Define measures
 
 # p_1 = ()->probability("same")
