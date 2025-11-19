74aca10dcee76e12c1b0907b3b7861a2b26ccfe9
giorgikanchaveli <gkanchaveli99@gmail.com>
2025-11-19 17:44:59 +0100

    asdasd

--- julia_run_files.sh
+++ julia_run_files.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=run_julia_file              # name of the SLURM job (shows up in queue)
 #SBATCH --partition=compute                    # which partition/queue to use
 #SBATCH --ntasks=1                           # number of tasks (1 is fine for Julia)
-#SBATCH --cpus-per-task=32						# number of cores
+#SBATCH --cpus-per-task=200						# number of cores
 #SBATCH --time=17:00:00                        # maximum run time (hh:mm:ss)
 #SBATCH --output=/home/3049277/logs/%x_%j.out  # standard output log file
 #SBATCH --error=/home/3049277/logs/%x_%j.err   # standard error log file
