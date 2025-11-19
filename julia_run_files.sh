cd2d14bf4d94d9f78957fb70560fc59f7e6e7259
giorgikanchaveli <gkanchaveli99@gmail.com>
2025-11-19 17:36:40 +0100

    changed julia run sh file for parallel computing

--- julia_run_files.sh
+++ julia_run_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -l
 #SBATCH --job-name=run_julia_file              # name of the SLURM job (shows up in queue)
 #SBATCH --partition=compute                    # which partition/queue to use
-#SBATCH --ntasks=1                             # number of tasks (1 is fine for Julia)
+#SBATCH --ntasks=1                           # number of tasks (1 is fine for Julia)
+#SBATCH --cpus-per-task=16						# number of cores
 #SBATCH --time=17:00:00                        # maximum run time (hh:mm:ss)
 #SBATCH --output=/home/3049277/logs/%x_%j.out  # standard output log file
 #SBATCH --error=/home/3049277/logs/%x_%j.err   # standard error log file
@@ -19,8 +20,9 @@ echo "Running from: $(pwd)"                    # print the current working direc
 
 # Run the Julia script using the environment defined by Project.toml in this directory
 # (gio.jl can contain 'using RCall' or any other Julia code)
+export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-julia --project=. -e 'using Pkg; Pkg.instantiate(); Pkg.precompile()'
-julia --project=. dm_enstat_hipm_wow.jl
+julia --project=. -e 'using Pkg; Pkg.instantiate(); Pkg.precompile(); println("Threads.nthreads() = ", Threads.nthreads())'
+#julia --project=. n_vs_m_hipm.jl
 
 echo "This is the end"                         # simple marker showing the script reached the end successfully
