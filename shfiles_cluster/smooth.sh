193760253686279cddbe07729ffc2b23a84e3b17
giorgikanchaveli <gkanchaveli99@gmail.com>
2025-12-03 18:37:00 +0100

    .sh

--- shfiles_cluster/smooth.sh
+++ shfiles_cluster/smooth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#SBATCH --job-name=smorest                 # name of the SLURM job (shows up in queue)
+#SBATCH --job-name=smo300                 # name of the SLURM job (shows up in queue)
 #SBATCH --partition=compute                    # which partition/queue to use
 #SBATCH --ntasks=1                           # number of tasks (1 is fine for Julia)
 #SBATCH --cpus-per-task=20    					# number of cores
-#SBATCH --time=05:00:00                        # maximum run time (hh:mm:ss)
+#SBATCH --time=35:00:00                        # maximum run time (hh:mm:ss)
 #SBATCH --output=/home/3049277/logs/%x_%j.out  # standard output log file
 #SBATCH --error=/home/3049277/logs/%x_%j.err   # standard error log file
 #SBATCH --chdir=/home/3049277/Two-samples-test # working directory for the job
