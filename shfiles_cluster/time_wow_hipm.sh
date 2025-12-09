309c82840eed0af25c15345acaacdecbbbc5f4c4
giorgikanchaveli <gkanchaveli99@gmail.com>
2025-12-09 12:17:04 +0100

    30

--- shfiles_cluster/time_wow_hipm.sh
+++ shfiles_cluster/time_wow_hipm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#SBATCH --job-name=time20                 # name of the SLURM job (shows up in queue)
+#SBATCH --job-name=time30                 # name of the SLURM job (shows up in queue)
 #SBATCH --partition=compute                    # which partition/queue to use
 #SBATCH --ntasks=1                           # number of tasks (1 is fine for Julia)
-#SBATCH --cpus-per-task=20    					# number of cores
+#SBATCH --cpus-per-task=30    					# number of cores
 #SBATCH --time=01:00:00                        # maximum run time (hh:mm:ss)
 #SBATCH --output=/home/3049277/logs/%x_%j.out  # standard output log file
 #SBATCH --error=/home/3049277/logs/%x_%j.err   # standard error log file
