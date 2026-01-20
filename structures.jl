b44c82afc785973fb82c51e7e839ff8d2e024ee7
giorgikanchaveli <gkanchaveli99@gmail.com>
2026-01-20 16:37:24 +0100

    changed names for struct, also it no longer needs arguments n and m.

--- structures.jl
+++ structures.jl
@@ -1,33 +1,56 @@
-# Every law of a random probability measure should be of type PPM. Then we define specific ways to generate 
+# Every law of a random probability measure(RPM) should be of type LawRPM. Then we define specific ways to generate 
 # hierarchical samples from these laws. 
 
-# Every hieraerchical sample should be of type emp_ppm, which contains the data (hierarchical sample) and some parameters, including number of
-# probability measures and number of observations from each probability measure, end points of the interval from which atoms are drawn.
+# Every hieraerchical sample should be of type HierSample, which contains the data (hierarchical sample) and the end points of the interval from which atoms are drawn.
 
 
 
 
-struct emp_ppm
+
+"""
+
+    HierSample(atoms, a, b)
+
+Object for a hierarchical sample generated from a law of RPM.
+    
+# Description: 
+        atoms: n x m matrix where each row i is generated from the latent probability measure P_i and P_1,...,P_n ~ law of RPM.
+        a: left end of the  interval containing all atoms
+        b: right end of the interval containing all atoms.
+
+# Arguments:
+        atoms::Matrix{<:Real}
+        a::<:Real
+        b::<:Real
+
+# Invariants: 
+        a <= b
+"""
+struct HierSample{T <: Real}
     # structure to store data for exchangeable sequences case
 
-    atoms::Matrix{Float64} # contains all observed random variables
-    n::Int # number of probability measures from which observations are generated
-    m::Int # number of observations from each probability measure
-    a::Float64 # interval [a,b] from which atoms are drawn
-    b::Float64 # interval [a,b] from which atoms are drawn
+    atoms::Matrix{T} # contains all observed random variables
+    a::T # interval [a,b] from which atoms are drawn
+    b::T # interval [a,b] from which atoms are drawn
 
-    function emp_ppm(atoms::Matrix{Float64}, n::Int, m::Int, a::Float64, b::Float64)
-        @assert size(atoms, 1) == n "n must equal the number of rows in atoms"
-        @assert size(atoms, 2) == m "m must equal the number of columns in atoms"
-        new(atoms, n, m, a, b)
+    function HierSample(atoms::Matrix{T}, a::T, b::T) where T
+        a <= b || throw(ArgumentError("a must be smaller than or equal to b, got a = $a, b = $b."))
+    
+        new{T}(atoms, a, b)
     end
 end
 
 
-abstract type PPM end # type for probability measure over space of probability measures
-# every law random probability measure should be a subtype of PPM because 
-# some functions will be defined on laws of random probability measure in general
+"""
+    LawRPM
 
+Type for all laws of Random probability measures.
 
+# Description: 
+
+Every law random probability measure should be a subtype of LawRPM because
+some functions will be defined on laws of random probability measure in general instead of on specific law.
+"""
+abstract type LawRPM end 
 
 
