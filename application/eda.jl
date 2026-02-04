d97f907003fcc1803466140351b6729df0cfdd67
giorgikanchaveli <gkanchaveli99@gmail.com>
2026-02-04 17:40:13 +0100

    xeploratory data analysis v2: added kde subplots.

--- application/eda.jl
+++ application/eda.jl
@@ -50,6 +50,43 @@ function frechet_mean(atoms::Matrix{Float64}, weights::Matrix{Float64})
     return dout,dsup
 end
 
+function frechet_means(atoms_per_year::Array{Float64,3}, weights_per_year::Array{Float64,3})
+    # atoms and weights are 3 dimensional arrays: n_time_periods x n_countries x n_ages
+
+    n_time_periods = size(atoms_per_year, 1)
+    n_countries = size(atoms_per_year, 2)
+        
+    alphas = collect(0.0:0.01:1.0)
+    quantiles_per_year = zeros(n_time_periods, n_countries, length(alphas))
+ 
+    for i in 1:n_time_periods
+        for j in 1:n_countries
+            pmf = DiscreteNonParametric(atoms_per_year[i, j,:], weights_per_year[i, j,:])
+            quantiles_per_year[i, j, :] = quantile.(Ref(pmf), alphas)
+        end
+    end
+
+    @rput quantiles_per_year n_time_periods alphas 
+    R"""
+    library(frechet)
+
+    dout_list <- list()
+    dsup_list <- list()
+    for (i in 1:n_time_periods) {
+        res = DenFMean(qin = quantiles_per_year[i, , ], optns = list(qSup = alphas))
+        dout_list[[i]] = res$dout
+        dsup_list[[i]] = res$dSup
+    }
+    """
+    @rget dout_list dsup_list
+    return dout_list, dsup_list
+end
+
+function kde_group(atoms::Matrix{Float64}, weights::Matrix{Float64}, min_age::Int, max_age::Int)
+    n_countries = size(atoms,1)
+    return [kde(atoms[i,:]; boundary = (min_age, max_age), weights = weights[i,:]) for i in 1:n_countries]
+end
+
 function plot_pmfs(gender_data::Dict{String, DataFrame}, groups::Tuple{Vector{String}, Vector{String}},
             t::Int, min_age::Int, max_age::Int)
     atoms_1, weights_1 = group_pmf_per_year(gender_data, groups[1], t, min_age, max_age)
@@ -70,6 +107,8 @@ function plot_pmfs(gender_data::Dict{String, DataFrame}, groups::Tuple{Vector{St
     return pl
 end
 
+
+
 function plot_kde(gender_data::Dict{String, DataFrame}, groups::Tuple{Vector{String}, Vector{String}},
             t::Int, min_age::Int, max_age::Int)
     atoms_1, weights_1 = group_pmf_per_year(gender_data, groups[1], t, min_age, max_age)
@@ -78,7 +117,7 @@ function plot_kde(gender_data::Dict{String, DataFrame}, groups::Tuple{Vector{Str
 
     n_countries_1, n_countries_2 = size(atoms_1, 1), size(atoms_2, 1)
     
-    pl = scatter(title = "KDE", xlabel = "age", ylabel = "density")
+    pl = plot(title = "KDE", xlabel = "age", ylabel = "density")
     for i in 1:n_countries_1
         label = (i == 1) ? "group1" : ""
         kde_country = kde(atoms_1[i,:]; boundary = (min_age, max_age), weights = weights_1[i,:])
@@ -107,60 +146,69 @@ function plot_frechetmean(gender_data::Dict{String, DataFrame}, groups::Tuple{Ve
     return pl
 end
 
-function plot_frechetmean(gender_data::Dict{String, DataFrame}, groups::Tuple{Vector{String}, Vector{String}},
+function plot_frechetmeans(gender_data::Dict{String, DataFrame}, groups::Tuple{Vector{String}, Vector{String}},
             time_periods::Vector{Int}, min_age::Int, max_age::Int)
 
-    pl = plot(title = "Frechet mean", xlabel = "age", ylabel = "density")
+    pl = plot(title = "Frechet mean", xlabel = "age", ylabel = "density", ylims = (0.0, 0.06))
     n_years = length(time_periods)
 
-    for (i, t) in enumerate(time_periods)
-        @info "Progress: $i / $(n_years)"
-        atoms_1, weights_1 = group_pmf_per_year(gender_data, groups[1], t, min_age, max_age)
-        atoms_2, weights_2 = group_pmf_per_year(gender_data, groups[2], t, min_age, max_age)
-        color_1, color_2 = "green", "brown"
+    # collect all atoms and weights
+    atoms_per_year_1 = zeros(n_years, length(groups[1]), max_age - min_age + 1)
+    weights_per_year_1 = zeros(n_years, length(groups[1]), max_age - min_age + 1)
 
-        dout_1, dsup_1 = frechet_mean(atoms_1, weights_1)
-        dout_2, dsup_2 = frechet_mean(atoms_2, weights_2)
-        intensity = 0.2 + (0.8 * (i / n_years))
+    atoms_per_year_2 = zeros(n_years, length(groups[2]), max_age - min_age + 1)
+    weights_per_year_2 = zeros(n_years, length(groups[2]), max_age - min_age + 1)
+
+    for (i, t) in enumerate(time_periods)
+        atoms_per_year_1[i, :, :], weights_per_year_1[i, :, :] = group_pmf_per_year(gender_data, groups[1], t, min_age, max_age)
+        atoms_per_year_2[i, :, :], weights_per_year_2[i, :, :] = group_pmf_per_year(gender_data, groups[2], t, min_age, max_age)
+        
+    end
+    dout_1, dsup_1 = frechet_means(atoms_per_year_1, weights_per_year_1)
+    dout_2, dsup_2 = frechet_means(atoms_per_year_2, weights_per_year_2)
+
+    for i in 1:n_years
+        intensity = 0.05 + (0.95 * (i / n_years))
         label = i == 1 ? "group1" : ""
-        plot!(pl, dsup_1, dout_1, color = color_1, label = label, alpha = intensity)
+        plot!(pl, dsup_1[i], dout_1[i], color = "green", label = label, alpha = intensity, ylims = (0.0, 0.06))
         label = i == 1 ? "group2" : ""
-        plot!(pl, dsup_2, dout_2, color = color_2, label = label, alpha = intensity)
+        plot!(pl, dsup_2[i], dout_2[i], color = "brown", label = label, alpha = intensity, ylims = (0.0, 0.06))
     end
     return pl
 end
+
 function plot_frechet_grid(gender_data::Dict{String, DataFrame}, 
                            groups::Tuple{Vector{String}, Vector{String}},
                            time_periods::Vector{Int}, min_age::Int, max_age::Int)
-
-    
     n_years = length(time_periods)
-    
-    # list of individual plots
-    plot_list = []
+
+    # collect all atoms and weights
+    atoms_per_year_1 = zeros(n_years, length(groups[1]), max_age - min_age + 1)
+    weights_per_year_1 = zeros(n_years, length(groups[1]), max_age - min_age + 1)
+
+    atoms_per_year_2 = zeros(n_years, length(groups[2]), max_age - min_age + 1)
+    weights_per_year_2 = zeros(n_years, length(groups[2]), max_age - min_age + 1)
 
     for (i, t) in enumerate(time_periods)
-        @info "Progess: $i / $(n_years)"
-        atoms_1, weights_1 = group_pmf_per_year(gender_data, groups[1], t, min_age, max_age)
-        atoms_2, weights_2 = group_pmf_per_year(gender_data, groups[2], t, min_age, max_age)
+        atoms_per_year_1[i, :, :], weights_per_year_1[i, :, :] = group_pmf_per_year(gender_data, groups[1], t, min_age, max_age)
+        atoms_per_year_2[i, :, :], weights_per_year_2[i, :, :] = group_pmf_per_year(gender_data, groups[2], t, min_age, max_age)
+    end
+    dout_1, dsup_1 = frechet_means(atoms_per_year_1, weights_per_year_1)
+    dout_2, dsup_2 = frechet_means(atoms_per_year_2, weights_per_year_2)
 
-        dout_1, dsup_1 = frechet_mean(atoms_1, weights_1)
-        dout_2, dsup_2 = frechet_mean(atoms_2, weights_2)
+    plot_list = []
+    for (i, t) in enumerate(time_periods)
 
-        
         # Create a subplot for this specific year
-        p = plot(title = "Year $t", titlefontsize=10, ylims = (0, 0.1)) 
+        p = plot(title = "Year $t", titlefontsize=10, ylims = (0.0, 0.06)) 
 
-        plot!(p, dsup_1, dout_1, color = "green", lw=2, label="group1")
-        plot!(p, dsup_2, dout_2, color = "brown", lw=2, label="group2")
+        plot!(p, dsup_1[i], dout_1[i], color = "green", lw=2, label="group1", ylims = (0.0, 0.06))
+        plot!(p, dsup_2[i], dout_2[i], color = "brown", lw=2, label="group2", ylims = (0.0, 0.06))
         
         push!(plot_list, p)
     end
-
-    
-    final_pl = plot(plot_list..., layout = (3, 3), size = (1200, 1200),
-                    xlabel = "Age", ylabel = "Density")
-
+    final_pl = plot(plot_list..., layout = (4, 4), size = (1200, 1200),
+                    xlabel = "Age", ylabel = "Density", ylims = (0.0, 0.06))
     return final_pl
 end
 
@@ -180,6 +228,48 @@ function plot_pooled_pmfs(gender_data::Dict{String, DataFrame}, groups::Tuple{Ve
     scatter!(pl, atoms_2, weights_2, color = "brown", label = "group2", alpha = 0.5)
     return pl
 end
+
+
+
+function plot_pooled_kdes_grid(gender_data::Dict{String, DataFrame}, 
+                           groups::Tuple{Vector{String}, Vector{String}},
+                           time_periods::Vector{Int}, min_age::Int, max_age::Int)
+
+    
+    n_years = length(time_periods)
+    
+    # list of individual plots
+    plot_list = []
+
+    for (i, t) in enumerate(time_periods)
+        @info "Progess: $i / $(n_years)"
+        atoms_1, weights_1 = group_pmf_per_year(gender_data, groups[1], t, min_age, max_age)
+        atoms_2, weights_2 = group_pmf_per_year(gender_data, groups[2], t, min_age, max_age)
+
+        kde_1 = kde_group(atoms_1, weights_1, min_age, max_age)
+        kde_2 = kde_group(atoms_2, weights_2, min_age, max_age)
+        
+        
+        # Create a subplot for this specific year
+        p = plot(title = "Year $t", titlefontsize=10, ylims = (0.0, 0.06)) 
+        for j in 1:length(kde_1)
+            lbl = (j == 1) ? "group1" : ""
+            plot!(p, kde_1[j].x, kde_1[j].density, color = "green", lw=2, label=lbl, alpha = 0.5, ylims = (0.0, 0.06))
+        end
+        for j in 1:length(kde_2)
+            lbl = (j == 1) ? "group2" : ""
+            plot!(p, kde_2[j].x, kde_2[j].density, color = "brown", lw=2, label=lbl, alpha = 0.5, ylims = (0.0, 0.06))
+        end 
+        push!(plot_list, p)
+    end
+
+    
+    final_pl = plot(plot_list..., layout = (4, 4), size = (1200, 1200),
+                    xlabel = "Age", ylabel = "Density", ylims = (0.0, 0.06))
+
+    return final_pl
+end
+
 
 group1 = ["belarus", "Bulgaria", "Czechia", "Estonia", "Hungary", "Latvia", "Poland", "Lithuania", "Russia", "Slovakia", "Ukraine"]
 
@@ -203,10 +293,22 @@ gender = "males"
 # pl_kdes = plot_kde(data_bank[gender], (group1, group2), t, min_age, max_age)
 
 # pl_frechet = plot_frechetmean(data_bank[gender], (group1, group2), t, min_age, max_age)
-pl_pooled = plot_pooled_pmfs(data_bank[gender], (group1, group2), t, min_age, max_age)
+# pl_pooled = plot_pooled_pmfs(data_bank[gender], (group1, group2), t, min_age, max_age)
 
-time_periods = collect(1960:6:2010)
-# pl_frechets = plot_frechetmean(data_bank[gender], (group1, group2), time_periods, min_age, max_age)
-# pl_new = plot_frechet_grid(data_bank[gender],  (group1, group2), time_periods, min_age, max_age)
+time_periods = collect(1963:3:2010)
+
+function save_plots(time_periods, gender)
+    pl_frechets_grid = plot_frechet_grid(data_bank[gender], (group1, group2), time_periods, min_age, max_age)
+    pl_frechetmeans = plot_frechetmeans(data_bank[gender], (group1, group2), time_periods, min_age, max_age)
+    pl_pooled_kdes = plot_pooled_kdes_grid(data_bank[gender], (group1, group2), time_periods, min_age, max_age)
+    output_dir = joinpath(pwd(), "applications", "plots")
+    mkpath(output_dir)
+    savefig(pl_pooled_kdes, joinpath(output_dir, "$(gender)_pooled_kdes.png"))
+    savefig(pl_frechets_grid, joinpath(output_dir, "$(gender)_frechets_grid.png"))
+    savefig(pl_frechetmeans, joinpath(output_dir, "$(gender)_frechets.png"))
+end
+save_plots(time_periods, "males")
+save_plots(time_periods, "females")
 print("done")
+
 
