b834b82c83c8626a546005f1a3a956238a8b7c17
giorgikanchaveli <gkanchaveli99@gmail.com>
2026-02-18 18:22:10 +0100

    checked pooling on simulated hierarchical samples

--- test_pooling.jl
+++ test_pooling.jl
@@ -5,8 +5,7 @@ include("methods.jl")
 # cp denotes classical pooling
 # np denotes new pooling
 
-
-function decision_np(hier_sample_1::HierSample, hier_sample_2::HierSample, θ::Float64, n_permutations::Int)
+function pvalue_np(hier_sample_1::HierSample, hier_sample_2::HierSample, n_permutations::Int)
     pooled_1 = vec(hier_sample_1.atoms)
     pooled_2 = vec(hier_sample_2.atoms)
     observed_distance = wasserstein_1d_equal(sort(pooled_1), sort(pooled_2))
@@ -24,9 +23,12 @@ function decision_np(hier_sample_1::HierSample, hier_sample_2::HierSample, θ::F
         new_pooled_2 = view(pooled, random_indices[n_1+1:n_total])
         samples[i] = wasserstein_1d_equal(sort(new_pooled_1), sort(new_pooled_2))
     end
-    p_value = mean(samples .>= observed_distance)
-    return Float64(p_value < θ)
+    return mean(samples .>= observed_distance)
 end
+function decision_np(hier_sample_1::HierSample, hier_sample_2::HierSample, θ::Float64, n_permutations::Int)
+    return Float64(pvalue_np(hier_sample_1, hier_sample_2, n_permutations)< θ)
+end
+
 function rejection_rate_cp_np(q_1::LawRPM, q_2::LawRPM, n::Int, m::Int, S::Int, θ::Float64,
                      n_permutations::Int)
     r_cp = 0.0
@@ -40,15 +42,22 @@ function rejection_rate_cp_np(q_1::LawRPM, q_2::LawRPM, n::Int, m::Int, S::Int, 
     return r_cp / S, r_np / S
 end
 
-q_1 = DP(1.0, Beta(1,13))
-q_2 = DP(1.0, Beta(1,13))  
+q_1 = DP(10000.0, Beta(1,1))
+q_2 = DP(10000.0, Beta(1,1))  
 n, m = 100, 100
 n_permutations = 100
 S = 100
 θ = 0.05
 t = time()
-r_cp, r_np = rejection_rate_cp_np(q_1, q_2, n, m, S, θ,n_permutations)
-dur = time() - t
-println("r_cp: $(r_cp)")
-println("r_np: $(r_np)")
+
+h_1 = generate_hiersample(q_1, n, m)
+h_2 = generate_hiersample(q_2, n, m)
+pvalue = pvalue_np(h_1, h_2, n_permutations)
+
+# r_cp, r_np = rejection_rate_cp_np(q_1, q_2, n, m, S, θ,n_permutations)
+# dur = time() - t
+# println("r_cp: $(r_cp)")
+# println("r_np: $(r_np)")
+
+
 
