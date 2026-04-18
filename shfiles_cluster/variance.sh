c857706a975f6872064f5204d775a2b3774db4a7
giorgikanchaveli <gkanchaveli99@gmail.com>
2026-04-18 20:16:17 +0200

    updated partition for sh files

--- shfiles_cluster/variance.sh
+++ shfiles_cluster/variance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --job-name=variance                        # name of the SLURM job (shows up in queue)
-#SBATCH --partition=compute                       # which partition/queue to use
+#SBATCH --partition=defq                       # which partition/queue to use
 #SBATCH --ntasks=1                                # number of tasks (1 is fine for Julia)
 #SBATCH --cpus-per-task=20  					  # number of cores
 #SBATCH --time=96:00:00                           # maximum run time (hh:mm:ss)
